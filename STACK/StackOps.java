5ecdbcacd0afff5c299e0192a73700dd6bf5361a
Ashtik Kumar <ashtik03229@gmail.com>
2024-02-27 11:50:04 +0530

    Added StackUnderFlowException, Made minor changes

--- STACK/StackOps.java
+++ STACK/StackOps.java
@@ -2,16 +2,22 @@ package DSA.STACK;
 
 import java.util.Scanner;
 
+class StackUnderFlowException extends RuntimeException{
+    public StackUnderFlowException(String e){
+        super(e);
+    }
+}
+
 class Stack {
     int stack[];
     int top;
-    Stack(){
-        stack = new int[100];
+    Stack(int n){
+        stack = new int[n];
         top = -1;
     }
 
     void push(int ele) throws StackOverflowError{
-        if(top==stack.length-1){
+        if(full()){
             throw new StackOverflowError("Stack Overflow !");
         } else {
             stack[++top] = ele;
@@ -19,17 +25,17 @@ class Stack {
         }
     }
 
-    int pop() throws ArrayIndexOutOfBoundsException{
+    int pop() throws StackUnderFlowException{
         if(empty()){
-            throw new ArrayIndexOutOfBoundsException("Stack Underflow !");
+            throw new StackUnderFlowException("Stack Underflow !");
         } else {
             return stack[top--];
         }
     }
 
-    void display() throws ArrayIndexOutOfBoundsException{
+    void display() throws StackUnderFlowException{
         if(empty()){
-            throw new ArrayIndexOutOfBoundsException("Stack is Empty !");
+            throw new StackUnderFlowException("Stack is Empty !");
         } else {
             for(int i=0; i<=top; i++){
                 System.out.print(stack[i]+" ");
@@ -37,47 +43,57 @@ class Stack {
         }
     }
 
-    int peek() throws ArrayIndexOutOfBoundsException{
+    int peek() throws StackUnderFlowException{
         if(empty()){
-            throw new ArrayIndexOutOfBoundsException("Stack is Empty !");
+            throw new StackUnderFlowException("Stack is Empty !");
         } else {
             return stack[top];
         }
     }
 
     boolean empty(){
-        if(top==-1) return true;
-        else return false;
+        if(top==-1)
+            return true;
+        return false;
+    }
+
+    boolean full(){
+        if(top==stack.length-1)
+            return true;
+        return false;
     }
 }
 
 public class StackOps {
     public static void main(String[] args) {
-        Stack stack = new Stack();
         Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Maximum capacity of Stack :");
+        Stack stack = new Stack(sc.nextInt());
+
         do{
             System.out.println("---MENU---");
             System.out.println("1. Push\n2. Pop\n3. Display\n4. Peek\n");
             System.out.print("Enter your choice : ");
             try{
                 switch(sc.nextInt()){
-                    case 1:System.out.println("Enter element to push : ");
+                    case 1:if(stack.full()) stack.push(0);
+                        System.out.print("Enter element to push : ");
                         stack.push(sc.nextInt());
                         break;
 
-                    case 2:System.out.println("Popped element :"+stack.pop());
+                    case 2:System.out.print("Popped element :"+stack.pop());
                         break;
 
                     case 3:stack.display();
                         break;
 
-                    case 4:System.out.println("Top element :"+stack.peek());
+                    case 4:System.out.print("Top element :"+stack.peek());
                         break;
 
-                    case 0:System.out.println("Exiting...");
+                    case 0:System.out.print("Exiting...");
                         break;
 
-                    default:System.out.println("Invalid Choice !");
+                    default:System.out.print("Invalid Choice !");
                 }
             } catch(StackOverflowError e){
                 System.out.println("Error : "+e.getMessage());
